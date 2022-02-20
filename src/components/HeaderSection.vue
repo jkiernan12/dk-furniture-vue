800e0193129c39b758d339b82d0291f4da300fb7
John Kiernan <jkiernan12@yahoo.com>
2022-02-20 13:43:11 -0500

    Add header styling

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -1,6 +1,11 @@
 <template>
   <header>
-    <p>David Kiernan Furniture Design</p>
+    <p class="logo">David Kiernan Furniture Design</p>
+    <nav>
+      <button>Portfolio</button>
+      <button>About</button>
+      <button>Contact</button>
+    </nav>
   </header>
 </template>
 
@@ -11,8 +16,41 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
   header {
     display: flex;
+    justify-content: space-between;
+    background-color: #7b0303;
+    color: white;
+    min-height: 100px;
+    margin: 0;
+
+    .logo {
+      font-size: 2em;
+      margin: 20px 50px;
+    }
+
+    nav {
+      display: flex;
+      align-items: center;
+      margin: 5px 20px;
+
+      button {
+        width: 33%;
+        padding: 5px 20px;
+        border: 1px solid white;
+        background-color: #7b0303;
+        color: white;
+        margin: 5px 10px;
+        font-size: 1.2em;
+        transition: .1s;
+        cursor: pointer;
+
+        &:hover {
+        color: #7b0303;
+        background-color: white;
+        }
+      }
+    }
   }
 </style>
