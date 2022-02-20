efa5281f853a7c1b6fc8c8b3201036527dc4ac25
John Kiernan <jkiernan12@yahoo.com>
2022-02-20 13:34:09 -0500

    Create first components

---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+    <p>David Kiernan Furniture Design</p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderSection'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  header {
+    display: flex;
+  }
+</style>
---
+++ src/components/SplashSection.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <p>David Kiernan has been making contemporary furniture in his Rhode Island studio since 1993. David, a former engineer, studied woodworking at the Worcester Center for Crafts.</p>
+    <p>"I find it exhilarating to use materials such as unique veneers, unusual stone, or patinated metals. Envisioning how to use these materials, provokes excitement and eagerness to proceed with a project. This to me is the essence of being a furniture maker; the process of discovery followed by transformation into a tangible, beautiful object."</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SplashSection',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
